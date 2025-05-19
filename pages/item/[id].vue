f575eb84a6ded991ce7ebace7575bf33a80b2a13
Saket-db <dbsaket04@gmail.com>
2025-05-19 16:54:27 +0530

    PageRoutesnEachImage

--- pages/item/[id].vue
+++ pages/item/[id].vue
@@ -1,9 +1,21 @@
 <template>
-    <div>
+    <MainLayout>
+        <div id="ItemPage" class="mt-4 max-w-[1200px] mx-auto px-2">
+            <div class="md:flex gap-4 justify-between mx-auto w-full">
+                <div class="md:w-[40%]">
+                    <img
+                        class="rounded-lg object-fit"
+                        src="https://picsum.photos/id/212/800/800"
+                    >
 
-    </div>
+                </div>
+            </div>
+        </div>
+    </MainLayout>
 </template>
 
 <script setup>
-
+import MainLayout from '~/layouts/MainLayout.vue';
 </script>
+
+I
